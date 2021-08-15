46a289c1776005fc1524ade41808024556f0ff28
emacsd <emacsd@icloud.com>
2021-08-15 16:47:59 -0400

    init

---
+++ include/my-header.h
@@ -0,0 +1,6 @@
+#ifndef _MYLIB_H_
+#define _MYLIB_H_
+
+int my_module_hello_world(void);
+
+#endif
---
+++ src/my-source.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2017 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "my-header.h"
+#include <stdio.h>
+
+int my_module_hello_world(void)
+{
+	printf("my-module says: Hello World!\n");
+	return 0;
+}
